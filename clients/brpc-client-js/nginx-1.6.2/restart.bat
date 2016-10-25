32ca9b49727e458b2b0cf1028db1814247969636
javeme <javaloveme@gmail.com>
2016-10-26 01:29:24 +0800

    js: improve nginx conf
    1improve nginx conf to avoid timeout: localhost -> 127.0.0.1
    2.clean up nginx start/stop bats

--- clients/brpc-client-js/nginx-1.6.2/restart.bat
+++ clients/brpc-client-js/nginx-1.6.2/restart.bat
@@ -1,4 +1,3 @@
-cd D:\c++\C++vs2010\brpc\clients\brpc-client-js\nginx-1.6.2
 call stop.bat
 call start.bat
 
--- clients/brpc-client-js/nginx-1.6.2/stop.bat
+++ clients/brpc-client-js/nginx-1.6.2/stop.bat
@@ -1,3 +1,2 @@
-cd D:\c++\C++vs2010\brpc\clients\brpc-client-js\nginx-1.6.2
 nginx.exe -s stop
 taskkill /F /IM nginx.exe
